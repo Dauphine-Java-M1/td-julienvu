8165e17ce4457b6c530d1998d0ba4c0665da3985
Julien <julienvu50@gmail.com>
2019-10-24 21:14:28 +0200

    exercice 2 td mvc advancing

---
+++ shapes/src/main/java/fr/dauphine/ja/vujulien/shapes/model/Drawable.java
@@ -0,0 +1,5 @@
+package fr.dauphine.ja.vujulien.shapes.model;
+
+public interface Drawable {
+	public void draw();
+}
--- shapes/src/main/java/fr/dauphine/ja/vujulien/shapes/model/Observable.java
+++ shapes/src/main/java/fr/dauphine/ja/vujulien/shapes/model/Observable.java
@@ -2,7 +2,8 @@ package fr.dauphine.ja.vujulien.shapes.model;
 
 import fr.dauphine.ja.vujulien.shapes.Observer;
 
-public interface Observable {
+public interface Observable { 
+	public void addObserver(Observer obs);
 	public void register(Observer obs);
 	public void unregister(Observer obs);
 	public void notifyObservers();
