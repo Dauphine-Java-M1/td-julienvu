ebd3facf8b4fce55fcd6a7913e441a269809fa6f
Julien <julienvu50@gmail.com>
2019-11-01 21:26:13 +0100

    Point.java

--- shapes/src/main/java/fr/dauphine/ja/vujulien/shapes/model/Point.java
+++ shapes/src/main/java/fr/dauphine/ja/vujulien/shapes/model/Point.java
@@ -1,48 +1,88 @@
-package fr.dauphine.ja.vujulien.shapes;
+package Exercice1;
 
 import java.util.ArrayList;
 
 public class Point {
-	private int x;
-	private int y;
-
-	public Point(int x, int y) {
+	//exercice 1 Point
+	private int x,y;
+	
+	private static int nombrepoints=0;
+	public Point(int x,int y) {
 		this.x=x;
 		this.y=y;
 	}
-	public Point() {
-		this.x=0;
-		this.y=0;
+	//question 8
+	//ne pas oublier this
+	public Point(Point p) {
+		this.x=p.x;
+		this.y=p.y;
+	}
+	//question 9
+	public String toString() {
+		return " ("+ this.x +"  ,"+this.y+ "  )";
+		
+	}
+	public static int getnombrepoints() {
+		return nombrepoints++;
+		
 	}
 	public int getX() {
 		return x;
 	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
 	public int getY() {
 		return y;
 	}
-
-	public void setY(int y) {
-		this.y = y;
+	public void setX(int x) {
+		this.x=x;
 	}
-	public static void main(String [ ]args) {
+	public void setY(int y) {
+		this.y=y;
+	}
+	//question 2 exo2
+	
+	public boolean isSameAs(Point p) {
+		if(this.x==p.x && this.y==p.y) {
+			return true;
+		}
+		return false;
+	}
+	//question 1 exercice 4 première méthode translate
+	public void translate(int dx,int dy) {
+		this.setX(this.x+dx);
+		this.setY(this.y+dy);
+	}
+	//question 1 exercice 4 deuxième méthode translate
+	public Point translate1(int dx,int dy) {
+		return new Point(x+dx,y+dy);
+	}
+	
+	public static void main(String[] args) {
+		Point p=new Point(1,2);
 		
-		Point p1=new Point(1,2);
-		Point p2=p1;
-		//Point p3=new Point(1,2);
-		Point p3=p1;
-		System.out.println(p1==p2);
 		
-		System.out.println(p1==p3);
+		nombrepoints++;
+		Point p4=new Point(new Point(1,9));
+		nombrepoints++;
+		System.out.println(Point.getnombrepoints());
+		System.out.println(p.toString());//(1, 2)
+		System.out.println(p4.toString());//(1 ,9)
+		//exercice 2
+		Point p2=p;
+		Point p3=new Point(1,2);
+		System.out.println(p==p2);//true
+		System.out.println(p==p3);//false
+		System.out.println(p2.isSameAs(p));//true
+		System.out.println(p3.isSameAs(p));//true
+		ArrayList<Point >list=new ArrayList();
+		list.add(p);
+		System.out.println(list.indexOf(p));//0
+		//System.out.println(list.indexOf(p3));//moins 1
 		
-		ArrayList<Point> list = new ArrayList<Point>();
-		list.add(p1);
-		System.out.println(list.indexOf(p2));
-		System.out.println(list.indexOf(p3));
 		
+		
+		
+	
+	
 	}
 	
 }
