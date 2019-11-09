f1c1fa5d53eff5989a531810f74f652e21ca2d46
Julien <julienvu50@gmail.com>
2019-11-09 08:07:51 +0100

    updating mydisplay file

--- shapes/src/main/java/fr/dauphine/ja/vujulien/shapes/view/MyDisplay.java
+++ shapes/src/main/java/fr/dauphine/ja/vujulien/shapes/view/MyDisplay.java
@@ -12,15 +12,18 @@ import fr.dauphine.ja.vujulien.shapes.model.Shape;
 import fr.dauphine.ja.vujulien.shapes.model.World;
 
 public class MyDisplay extends JPanel {
-	static World world;
+	World world;
 	ArrayList<Shape> shapes;
 	public MyDisplay(World w) {
 		this.world=w;
 		//this.world.addObserver(this);
 	}
+	public MyDisplay(){
+		this.world=new World();
+	}
 	public void painComponent(Graphics g) {
-		for(Shape s:shapes) {
-			//s.draw();
+		for(Shape s:this.world.getW()) {
+			s.draw(g);
 			
 		}
 	}
@@ -29,10 +32,19 @@ public class MyDisplay extends JPanel {
 		frame.setSize(new Dimension(500,500));
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		MyDisplay d = new MyDisplay(world);
+		
+		World world = new World();
+		Point p1 = new Point(200,600);
+		Point p2 = new Point(540,510);
+		LigneBrisee l = new LigneBrisee();
+		l.add(p1);
+		Circle c=new Circle(p1,150);
+		Circle c2=new Circle(p2,100);
+		world.add(l);
+		world.add(c2);
+		System.out.println(world);
+		MyDisplay d=new MyDisplay(world);
 		frame.add(d);
-		//Graphics g = null;
-		//d.paintComponent(g);
 	
 	}
 		
