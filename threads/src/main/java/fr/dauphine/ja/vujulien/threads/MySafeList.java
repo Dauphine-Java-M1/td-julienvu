055f4083e7f421ae5a016616b76eca3e6d04b176
julienvu <julienvu50@gmail.com>
2019-12-18 21:09:45 +0100

    Update

--- threads/src/main/java/fr/dauphine/ja/vujulien/threads/MySafeList.java
+++ threads/src/main/java/fr/dauphine/ja/vujulien/threads/MySafeList.java
@@ -19,15 +19,19 @@ public class MySafeList {
 		
 	}
 	
+	//méthode ajout de doubles
 	public synchronized void add (Double d) {
 		
 			l.add(d);
 		
 	}
+	
+	//méthode size
 	public synchronized int size() {
 		
 		return l.size();
 	}
+	//méthode d'accès à un élément de la liste l
 	public synchronized Double get(int position) {
 		
 		return l.get(position);
@@ -36,7 +40,7 @@ public class MySafeList {
 	
 	//Exercice 3 question 1
 	public static double scalar(MySafeList v1,MySafeList v2) {
-		
+		//si v1 et v2 de tailles différentes
 		if(v1.size()!=v2.size()) {
 			throw new IllegalArgumentException("v1 et v2 doivent être de même dimension");
 		}
@@ -59,7 +63,7 @@ public class MySafeList {
 	
 	public static double scalarAlea(MySafeList l1,MySafeList l2) {
 		//avec vecteurs aléatoires
-		
+		//si v1 et v2 de tailles différentes
 		if(l1.size()!=l2.size()) {
 			throw new IllegalArgumentException("v1 et v2 doivent être de même dimension");
 		}
@@ -86,11 +90,13 @@ public class MySafeList {
 	
 	
 		public static double scalarParallele(MySafeList l1,MySafeList l2,int begin,int end) {
-			//sans argument
 			
+			//sans argument
+			//si v1 et v2 de tailles différentes
 			if(l1.size()!=l2.size()) {
 				throw new IllegalArgumentException("v1 et v2 doivent être de même dimension");
 			}
+			//si begin est un indice négatif et end supérieure à la taille maximale de la liste
 			if(begin<0 || end> n) {
 				throw new IllegalArgumentException("begin et end must be redefined");
 			}
@@ -102,7 +108,7 @@ public class MySafeList {
 				  	l1.add(suiv);
 				  	l2.add(suiv);
 			}
-			//produit scalaire
+			//produit scalaire entre l1 et l2
 			for(int indvec=begin;indvec<=end;indvec++) {
 				
 				prod += l1.get(indvec)*l2.get(indvec);
@@ -117,18 +123,21 @@ public class MySafeList {
 		List<Thread>t=new ArrayList<Thread>();
 		try {
 				synchronized(t) {
-				for(int i=0;i<n;i++) {
-					Thread t1=new Thread();
-					t.add(t1);
-					t1.start();
-					t1.join();
-				}
-				for(int j=0;j<m;j++) {
-					l.add((double) j);
-				}
-				if(l.size()!=n*m) {
-					return false;
-				}
+					for(int i=0;i<n;i++) {
+						Thread t1=new Thread();
+						t.add(t1);
+						t1.start();
+						t1.join();
+					}
+					
+					//ajout des m entiers 
+					for(int j=0;j<m;j++) {
+						l.add((double) j);
+					}
+					//si taille de l différent du produit n*m
+					if(l.size()!=n*m) {
+						return false;
+					}
 				}
 		}catch(InterruptedException e) {
 				e.printStackTrace();
@@ -138,6 +147,9 @@ public class MySafeList {
 			
 	}
 	public static void main(String []args) {
+		
+		
+		//test question 3 exercice 2
 		MySafeList l=new MySafeList(3);
 		l.add(2.1);
 		l.add(234.23);
@@ -146,8 +158,8 @@ public class MySafeList {
 		System.out.println("résultat de stressTest avec n=2 et m=6: "+l.stressTest(2,6));//true
 		System.out.println("résultat de stressTest avec n=2 et m=1: "+l.stressTest(2,1));//false
 		
-		//Exercice 3 question 1
 		
+		//test question 1 exercice 3
 		MySafeList v1=new MySafeList(3);
 		MySafeList v2=new MySafeList(3);
 		
@@ -161,7 +173,7 @@ public class MySafeList {
 		v2.add(1.0);
 		System.out.println(MySafeList.scalar(v1, v2));//9.0
 		
-		
+		//test question 1 exercice 3
 		MySafeList v4=new MySafeList(4);
 		MySafeList v5=new MySafeList(4);
 		v4.add(3.0);
@@ -174,16 +186,16 @@ public class MySafeList {
 		v5.add(5.0);
 		System.out.println("le produit scalaire entre 0 et n-1 entre deux vecteurs arbitraires vaut: "+MySafeList.scalar(v4, v5));//30.0
 		
-		//test question 2
+		//test question 2 exercice 3
 		MySafeList l1=new MySafeList(n);
 		MySafeList l2=new MySafeList(n);
 		System.out.println("le produit scalaire entre 0 et n-1 entre deux vecteurs aléatoires vaut: "+MySafeList.scalarAlea(l1,l2));
 		
 		
-		//test question 3
+		//test question 3 exercice 3
 		MySafeList l3=new MySafeList(n);
 		MySafeList l4=new MySafeList(n);
-		System.out.println("le produit scalaire entre begin et end vaut: "+  MySafeList.scalarParallele(l3,l4, 1, 2));
+		System.out.println("le produit scalaire entre begin et end vaut: "+  MySafeList.scalarParallele(l3,l4, 1, 3));
 
 
 		
